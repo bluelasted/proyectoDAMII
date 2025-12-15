58d2ea7d2abc50e0a806d11d1a2d5d5bbd623bc7
bluelasted <mastrohos3@gmail.com>
2025-12-14 23:01:37 -0500

    modulo de areas completo y avance para modulo de usuarios (faltan validaciones)

---
+++ RRHH/Models/Usuario.swift
@@ -0,0 +1,9 @@
+//
+//  Usuario.swift
+//  RRHH
+//
+//  Created by Percy Valverde on 14/12/25.
+//  Copyright © 2025 Los malditos de cibertec. All rights reserved.
+//
+
+import Foundation
---
+++ RRHH/Services/AreaService.swift
@@ -0,0 +1,9 @@
+//
+//  AreaService.swift
+//  RRHH
+//
+//  Created by Percy Valverde on 14/12/25.
+//  Copyright © 2025 Los malditos de cibertec. All rights reserved.
+//
+
+import Foundation
